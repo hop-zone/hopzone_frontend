9d28c8f1f38ca420058ad3ca7fb6089451038eff
Alexander Everaert <alexander.everaert@student.howest.be>
2022-01-10 12:02:47 +0100

    finish next setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className=" text-red-500 text-center">Hello World!</h1>
-      <p className=' text-2xl'>rtygbcnarjhkgtc</p>
+      <h1 className=" text-red-900 text-center">Hello World!</h1>
     </div>
   )
 }
