45f1d66f4d6fc524034965c21ea0a4039cbe637d
Alexander Everaert <alexander.everaert@student.howest.be>
2022-01-10 12:00:53 +0100

    tailwind config and imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className=" text-red-900 text-center">Hello World!</h1>
+      <h1 className=" text-red-500 text-center">Hello World!</h1>
+      <p className=' text-2xl'>rtygbcnarjhkgtc</p>
     </div>
   )
 }
