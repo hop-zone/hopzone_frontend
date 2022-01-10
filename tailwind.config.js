56f70dff7216ad98107cafe6e785f45370f5277e
EveraertAlexander <alexander.everaert@student.howest.be>
2022-01-10 11:47:18 +0100

    edited tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
+  // mode: 'jit',
+
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+
+  darkMode: 'media', // or 'class'
+
   theme: {
     extend: {},
   },
+
+  variants: {
+    extend: {},
+  },
+
   plugins: [],
 }
